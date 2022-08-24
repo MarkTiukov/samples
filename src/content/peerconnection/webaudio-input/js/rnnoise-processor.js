bb41c5be623f038b4376a1e257a68b8b81c97c86
Mark Tyukov <tyukov.mark@gmail.com>
2022-08-24 14:56:37 +0300

    v1

--- src/content/peerconnection/webaudio-input/js/rnnoise-processor.js
+++ src/content/peerconnection/webaudio-input/js/rnnoise-processor.js
@@ -1,9 +1,6 @@
 'use strict';
 
-// import { Rnnoise } from "/@shiguredo/rnnoise-wasm";
-// import { Rnnoise } from "/node_modules/@shiguredo/rnnoise-wasm/dist/rnnoise.mjs";
-
-import { createRNNWasmModuleSync } from "/node_modules/@jitsi/rnnoise-wasm/dist/rnnoise-sync.js";
+import createRNNWasmModuleSync from "/node_modules/@jitsi/rnnoise-wasm/dist/rnnoise-sync.js";
 
 class RnnoiseProcessor extends AudioWorkletProcessor {
 
@@ -17,14 +14,8 @@ class RnnoiseProcessor extends AudioWorkletProcessor {
     constructor() {
         super();
         console.log("creating rnnoise module");  
-        
-        // Rnnoise.load().then((rnnoise) => {
-        //     console.log("callback");
-        //     this.rnnoise = rnnoise;
-        //     this.denoisedState = this.rnnoise.createDenoiseState();
-        // });
-
         this.rnnoiseWasmModule = createRNNWasmModuleSync();
+        this.denoise_state = this.rnnoiseWasmModule._rnnoise_create();
     }
 
     generateWhiteNoise(output) {
@@ -42,15 +33,20 @@ class RnnoiseProcessor extends AudioWorkletProcessor {
     }
 
     processWithRnnoise(input, output) {
-        // for (let i = 0; i < output[0].length; i++) {
-        //     output[0][i] = input[0][i];
-        //     this.rnnoise.processFrame(output[0][i]);
-        // }
-        console.log(`done`);
+        for (let i = 0; i < input[0].length; i++) {
+            // output[0][i] = input[0][i];
+            output[1][i] = 0;    
+        }
+        let bufferIn = new Float32Array(480);
+        let bufferOut = new Float32Array(480);
+        bufferIn.set(input[0]);
+        this.rnnoiseWasmModule._rnnoise_process_frame(this.denoise_state, bufferOut, bufferIn);
+        console.log(`buffer output length == ${bufferOut.length}, output length == ${output[0].length}`);
+        output[0].set(bufferOut.slice(0, 128));
     }
     
     process(inputs, outputs, parameters) {
-        console.log(`hello from rnnoise: ${inputs.length}, ${outputs.length}`);
+        // console.log(`hello from rnnoise: ${inputs.length}, ${outputs.length}`);
         const input = inputs[0];
         const output = outputs[0];
 
