19dce1c4a0374f1ca71d7d9a3ff3881bc9e349ac
Mark Tyukov <tyukov.mark@gmail.com>
2022-08-26 15:29:10 +0300

    memory troubles

--- src/content/peerconnection/webaudio-input/js/rnnoise-processor.js
+++ src/content/peerconnection/webaudio-input/js/rnnoise-processor.js
@@ -33,16 +33,25 @@ class RnnoiseProcessor extends AudioWorkletProcessor {
     }
 
     processWithRnnoise(input, output) {
-        for (let i = 0; i < input[0].length; i++) {
-            // output[0][i] = input[0][i];
-            output[1][i] = 0;    
+        
+        // TODO: завести массив для Си
+        let inArray = this.rnnoiseWasmModule._malloc(480 * 4);
+        if (this.rnnoiseWasmModule.wasmMemory == undefined) {
+            console.log(`wasm memorry is undefinied`);
         }
-        let bufferIn = new Float32Array(480);
-        let bufferOut = new Float32Array(480);
-        bufferIn.set(input[0]);
-        this.rnnoiseWasmModule._rnnoise_process_frame(this.denoise_state, bufferOut, bufferIn);
-        console.log(`buffer output length == ${bufferOut.length}, output length == ${output[0].length}`);
-        output[0].set(bufferOut.slice(0, 128));
+        let wasmMem = new Float32Array(this.rnnoiseWasmModule.wasmMemory.buffer, inArray, 480);
+        wasmMem.set(input[0]);
+
+        // let bufferIn = new Float32Array(480);
+        // let bufferOut = new Float32Array(480);
+        // bufferIn.set(input[0]);
+
+        // this.rnnoiseWasmModule._rnnoise_process_frame(
+        //     this.denoise_state, bufferOut, bufferIn);
+        // console.log(`buffer output length == ${bufferOut.length}, output length == ${output[0].length}`);
+        // output[0].set(bufferOut.slice(0, 128));
+        // console.log(bufferOut);
+        output[0].set(input[0], 0, 128);
     }
     
     process(inputs, outputs, parameters) {
