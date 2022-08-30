d58bae1a2887588dae9b01d4c928ce0de284b8a7
Mark Tyukov <tyukov.mark@gmail.com>
2022-08-30 17:48:06 +0300

    typo fix

--- src/content/peerconnection/webaudio-input/js/rnnoise-processor.js
+++ src/content/peerconnection/webaudio-input/js/rnnoise-processor.js
@@ -53,7 +53,7 @@ class RnnoiseProcessor extends AudioWorkletProcessor {
 
     processWithRnnoise(input, output) {
         for (let i = 0; i < input[0].length; i++) {
-            this.rnnModule.HEAPF32[this.inputBufferF32Index + i] = input[i];
+            this.rnnModule.HEAPF32[this.inputBufferF32Index + i] = input[0][i];
         }
 
         for (let i = input[0].length; i < RNNOISE_SAMPLE_LENGTH; i++) {
@@ -69,6 +69,27 @@ class RnnoiseProcessor extends AudioWorkletProcessor {
         for (let i = 0; i < input[0].length; i++) {
             output[0][i] = this.rnnModule.HEAPF32[this.inputBufferF32Index + i];
         }
+
+        console.log(output[0]);
+
+        // let inArray = this.rnnModule._malloc(RNNOISE_BUFFER_SIZE);
+        // if (this.rnnModule.wasmMemory == undefined) {
+        //     console.log(`wasm memorry is undefinied`);
+        // }
+        // let wasmMem = new Float32Array(this.rnnModule.wasmMemory.buffer, inArray, RNNOISE_SAMPLE_LENGTH);
+        // wasmMem.set(input[0]);
+
+        // let bufferIn = new Float32Array(480);
+        // let bufferOut = new Float32Array(480);
+        // bufferIn.set(input[0]);
+
+        // this.rnnoiseWasmModule._rnnoise_process_frame(
+        //     this.denoise_state, bufferOut, bufferIn);
+        // console.log(`buffer output length == ${bufferOut.length}, output length == ${output[0].length}`);
+        // output[0].set(bufferOut.slice(0, 128));
+        // console.log(bufferOut);
+        // output[0].set(input[0], 0, 128);
+
     }
     
     process(inputs, outputs, parameters) {
